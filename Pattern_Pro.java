3812cb5fd74b30338b49a241896d54a4809605d5
Maxar-maker <arjun.v.doss@gmail.com>
2025-09-27 14:41:16 +0530

    Making a inverted triangle

---
+++ Pattern_Pro.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Pattern_Pro
+{
+    public static void main()
+    {
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=i;j<=5;j++)
+            {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
