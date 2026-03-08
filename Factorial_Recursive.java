6dfa30cd2c3a6fe12101eb26661d4d8457dc47fd
Maxar-maker <arjun.v.doss@gmail.com>
2026-03-08 15:28:04 +0530

    Find_Factorials

---
+++ Factorial_Recursive.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Factorial_Recursive
+{
+    
+    public int fact(int num)
+    {
+        if(num==1)
+        {
+            return 1;
+        }
+        else
+        {
+            return(num*fact(num-1));
+        }
+    }
+}
